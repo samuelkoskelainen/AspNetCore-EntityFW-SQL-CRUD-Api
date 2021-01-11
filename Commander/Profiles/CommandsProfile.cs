7aa8ae9cb3e2b80e713e14f325dc823c9d7c5f48
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2021-01-11 17:32:59 +0200

    Add project files.

---
+++ Commander/Profiles/CommandsProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Get
+            // Source - Target
+            CreateMap<Command, CommandReadDto>();
+
+            // Post
+            // Target - Source
+            CreateMap<CommandCreateDto, Command>();
+
+            // Put
+            // Target - Source
+            CreateMap<CommandUpdateDto, Command>();
+
+            // Patch
+            // Source - Target
+            CreateMap<Command, CommandUpdateDto>();
+        }
+    }
+}
